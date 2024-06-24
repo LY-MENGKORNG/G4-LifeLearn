d6b0ec7fbbb347c9879e55bb330c1cbe18b1ae76
SAMORN.MAI <SAMORN.MAI@PNC.LAN>
2024-06-24 16:17:11 +0700

    feature] build api score, comment, create table semester on migrate and resource , and then update course, subject VC2-58

--- backend/app/Models/Quizze.php
+++ backend/app/Models/Quizze.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class Quizze extends Model
+class Quizze extends RelationshipModel
 {
     use HasFactory;
     protected $fillable = ['subject_id'];
