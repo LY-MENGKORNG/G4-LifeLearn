03c049a6b9a3f4c787bb87ad8e8deb749b963920
Your Name <CHANNY.CHHUN@PNC.LAN>
2024-06-26 18:58:35 +0700

    VC-88 [feature] book

--- frontend/src/router/routes.ts
+++ frontend/src/router/routes.ts
@@ -2,7 +2,7 @@ const routes = [
     {
         path: '/admin/dashboard',
         name: 'dashboard',
-        component: () => import('@/views/Admin/DashboardView.vue'),
+        component: () => import('../views/Admin/DashboardView.vue'),
         meta: {
             requiresAuth: true,
             role: 'admin'
@@ -11,12 +11,17 @@ const routes = [
     {
         path: '/login',
         name: 'login',
-        component: () => import('@/views/Admin/Auth/LoginView.vue')
+        component: () => import('../views/Admin/Auth/LoginView.vue')
     },
     {
         path: '/',
         name: 'home',
         component: () => import('../views/Web/HomeView.vue')
+    },
+    {
+        path: '/book',
+        name: 'book',
+        component: () => import('../views/Web/Book/ListBookView.vue')
     }
 ]
 
---
+++ frontend/src/views/Web/Book/ListBookView.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        this is book page
+    </div>
+</template>
+<script setup lang="ts">
+
+</script>
