cea0836d834fc4a18f796f3301425af57013dab9
Your Name <CHANNY.CHHUN@PNC.LAN>
2024-06-26 08:46:54 +0700

    VC2-88 [feature] continue on book page

--- frontend/src/router/routes.ts
+++ frontend/src/router/routes.ts
@@ -2,7 +2,7 @@ const routes = [
     {
         path: '/admin/dashboard',
         name: 'dashboard',
-        component: () => import('../views/Admin/DashboardView.vue'),
+        component: () => import('@/views/Admin/DashboardView.vue'),
         meta: {
             requiresAuth: true,
             role: 'admin'
@@ -11,7 +11,7 @@ const routes = [
     {
         path: '/login',
         name: 'login',
-        component: () => import('../views/Admin/Auth/LoginView.vue')
+        component: () => import('@/views/Admin/Auth/LoginView.vue')
     },
     {
         path: '/',
