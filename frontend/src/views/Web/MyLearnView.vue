ec2bee8c7a6c9fb464a21a45ddc18121bee14c70
SAMORN.MAI <SAMORN.MAI@PNC.LAN>
2024-06-27 16:28:25 +0700

    [feat]create list all books on frond end VC2-98

--- frontend/src/views/Web/MyLearnView.vue
+++ frontend/src/views/Web/MyLearnView.vue
@@ -1,9 +1,158 @@
 <template>
-    <WebLayout>
-
-    </WebLayout>
+  <WebLayout>
+    <div class="book-list">
+      <h1 class="mt-5 text-center text-teal-400">All Books</h1>
+        <div class="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-5 gap-x-1 gap-y-1">
+            <div
+                class="rounded-lg bg-white border text-center shadow-secondary-1 dark:bg-surface-dark dark:text-white text-surface mt-5 max-w-[18rem] max-h-[21rem] overflow-hidden hover:transform hover:-translate-y-5 transition duration-300 ease-in-out"
+            >
+                <img
+                src="https://rochizalani.com/wp-content/uploads/2021/04/psychology-of-money.jpg"
+                alt="Description of image"
+                class="w-full h-[200px] object-cover rounded-t-lg"
+                />
+                <div class="p-6">
+                    <h5 class="mb-2 text-lg font-medium leading-tight text-start">
+                        <b>The Psychology of Money</b>
+                    </h5>
+                    <p class="mb-2 text-base text-start py-1 min-w-[18rem]">
+                        Magan Housal
+                        <span class="text-yellow-400 ml-10">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        </span>
+                    </p>
+                </div>
+            </div>
+             <div
+                class="rounded-lg bg-white border text-center shadow-secondary-1 dark:bg-surface-dark dark:text-white text-surface mt-5 max-w-[18rem] max-h-[21rem] overflow-hidden hover:shadow-md hover:transform hover:-translate-y-5 transition duration-300 ease-in-out"
+            >
+                <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBhPwev33jsXrDo7ygUaFZpn8C4xaRxz1fUw&s"
+                alt="Description of image"
+                class="w-full h-[200px] object-cover rounded-t-lg"
+                />
+                <div class="p-6">
+                    <h5 class="mb-2 text-lg font-medium leading-tight text-start">
+                        <b>The Psychology of Money</b>
+                    </h5>
+                    <p class="mb-2 text-base text-start py-1 min-w-[18rem]">
+                        Magan Housal
+                        <span class="text-yellow-400 ml-10">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        </span>
+                    </p>
+                </div>
+            </div>
+            <div
+                class="rounded-lg bg-white border text-center shadow-secondary-1 dark:bg-surface-dark dark:text-white text-surface mt-5 max-w-[18rem] max-h-[21rem] overflow-hidden hover:shadow-md hover:transform hover:-translate-y-5 transition duration-300 ease-in-out"
+            >
+                <img
+                src="https://rochizalani.com/wp-content/uploads/2021/04/psychology-of-money.jpg"
+                alt="Description of image"
+                class="w-full h-[200px] object-cover rounded-t-lg"
+                />
+                <div class="p-6">
+                    <h5 class="mb-2 text-lg font-medium leading-tight text-start">
+                        <b>The Psychology of Money</b>
+                    </h5>
+                    <p class="mb-2 text-base text-start py-1 min-w-[18rem]">
+                        Magan Housal
+                        <span class="text-yellow-400 ml-10">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        </span>
+                    </p>
+                </div>
+            </div>
+            <div
+                class="rounded-lg bg-white border text-center shadow-secondary-1 dark:bg-surface-dark dark:text-white text-surface mt-5 max-w-[18rem] max-h-[21rem] overflow-hidden hover:shadow-md hover:transform hover:-translate-y-5 transition duration-300 ease-in-out"
+            >
+                <img
+                src="https://rochizalani.com/wp-content/uploads/2021/04/psychology-of-money.jpg"
+                alt="Description of image"
+                class="w-full h-[200px] object-cover rounded-t-lg"
+                />
+                <div class="p-6">
+                    <h5 class="mb-2 text-lg font-medium leading-tight text-start">
+                        <b>The Psychology of Money</b>
+                    </h5>
+                    <p class="mb-2 text-base text-start py-1 min-w-[18rem]">
+                        Magan Housal
+                        <span class="text-yellow-400 ml-10">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        </span>
+                    </p>
+                </div>
+            </div>
+            <div
+                class="rounded-lg bg-white border text-center shadow-secondary-1 dark:bg-surface-dark dark:text-white text-surface mt-5 max-w-[18rem] max-h-[21rem] overflow-hidden hover:shadow-md hover:transform hover:-translate-y-5 transition duration-300 ease-in-out"
+            >
+                <img
+                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSGkpRA2IF0ShWq5ooipCFt5NDOZl9OrXbgPgfGYpncaqj4sg8mOyQOXI1TuEWQTDbzEZQ&usqp=CAU"
+                alt="Description of image"
+                class="w-full h-[200px] object-cover rounded-t-lg "
+                />
+                <div class="p-6">
+                    <h5 class="mb-2 text-lg font-medium leading-tight text-start">
+                        <b>The Psychology of Money</b>
+                    </h5>
+                    <p class="mb-2 text-base text-start py-1 min-w-[18rem]">
+                        Magan Housal
+                        <span class="text-yellow-400 ml-10">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        </span>
+                    </p>
+                </div>
+            </div>
+            <div
+                class="rounded-lg bg-white border text-center shadow-secondary-1 dark:bg-surface-dark dark:text-white text-surface mt-5 max-w-[18rem] max-h-[21rem] overflow-hidden hover:shadow-md hover:transform hover:-translate-y-5 transition duration-300 ease-in-out"
+            >
+                <img
+                src="https://rochizalani.com/wp-content/uploads/2021/04/psychology-of-money.jpg"
+                alt="Description of image"
+                class="w-full h-[200px] object-cover rounded-t-lg"
+                />
+                <div class="p-6">
+                    <h5 class="mb-2 text-lg font-medium leading-tight text-start">
+                        <b>The Psychology of Money</b>
+                    </h5>
+                    <p class="mb-2 text-base text-start py-1 min-w-[18rem]">
+                        Magan Housal
+                        <span class="text-yellow-400 ml-10">
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        <i class="fas fa-star"></i>
+                        </span>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+  </WebLayout>
 </template>
 
 <script setup lang="ts">
-import WebLayout from '@/Layouts/Web/WebLayout.vue';
+import WebLayout from '@/Layouts/Web/WebLayout.vue'
 </script>
