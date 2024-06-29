03999ed74efd860dbd2f543593e63f6cf2b88717
Your Name <CHANNY.CHHUN@pnc.lan>
2024-06-29 21:52:54 +0700

    VC2-88 [feature] feature form book

--- frontend/src/views/Web/BookCreateView.vue
+++ frontend/src/views/Web/BookCreateView.vue
@@ -1,6 +1,6 @@
 <template>
     <WebLayout>
-        <div @submit="submitForm"  class="w-full max-w-xl m-auto p-3  bg-gray-200">
+      <div  class="w-full max-w-xl m-auto p-3  bg-gray-200">
             <form class=" shadow-md rounded px-8 pt-6 pb-8 mb-4 bg-gray-100 ">
             <p class="text-center font-bold mb-4 text-teal-500">Create Books</p>
             <div class="mb-4">
@@ -11,7 +11,7 @@
             id="title" 
             type="text"
              placeholder="Title"
-             v-model="newBooks.title"
+            
              >
             </div>
             <div class="mb-4">
@@ -22,7 +22,7 @@
             id="author" 
             type="text" 
             placeholder="Author"
-            v-model="newBooks.author"
+            
             >
             </div>
             <div class="mb-4">
@@ -33,7 +33,7 @@
             id="user_id" 
             type="number" 
             placeholder="User_id"
-            v-model="newBooks.user_id"
+            
             > 
             </div>
             <div class="mb-4">
@@ -44,7 +44,7 @@
             id="published_at" 
             type="date" 
             placeholder="Published_at"
-            v-model="newBooks.published_at">
+            >
 
             </div>
             <div class="mb-4">
@@ -55,7 +55,7 @@
             id="price" 
             type="number" 
             placeholder="Price"
-            v-model="newBooks.price"
+            
             >
             </div>
             <div class="flex items-center justify-between">
@@ -75,37 +75,9 @@
 </template>
 
 <script>
-import axios from "axios";
+
 import WebLayout from '@/Layouts/Web/WebLayout.vue';
-export default {
-    name: "create-books",
-  data() {
-    return {
-      books: {
-        title:"",
-        author:"",
-        user_id:"",
-        published_at:"",
-        price:"",
-      },
-    };
-  },
-  methods: {
-    async submitForm() {
-      try {
-        const response = await axios.post(
-          "http://127.0.0.1:8000/api/books",
-          this.books
-        );
-        if (response.data.success) {
-          this.$router.push("/books");
-        }
-      } catch (error) {
-        console.error("Error creating book:", error);
-      }
-    },
-  },
-};
+
 
 </script>
 
