416cec3c1c1b15b65ffab06ba64cc04ae7f9a6cf
vorn savorn <savorn.vorn@student.passerellesnumeriques.org>
2024-07-23 09:13:35 +0700

    VC2-171 [feature]: Intergarte API permission with frontend

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -209,7 +209,7 @@ Route::middleware('auth:sanctum')->group(function () {
 
     // event 
     Route::get('/events', [EventController::class,  'index']);
-    Route::post('events', [EventController::class, 'store']);
+    Route::post('/events', [EventController::class, 'store']);
     Route::put('events/{id}', [EventController::class, 'update']);
     Route::delete('events/{id}', [EventController::class, 'destroy']);
 
